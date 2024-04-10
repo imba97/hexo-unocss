72171269e49e736cfa772ebe020a3104923fcf4c
imba97 <mail@imba97.cn>
2024-04-10 21:57:32 +0800

    feat: unocss 可配置

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
-import { once } from '@/utils'
+import fs from 'node:fs'
+import unocss from 'unocss'
+import { resolve } from 'node:path'
 import { exec } from 'node:child_process'
+
+import { get, once } from '@/utils'
 
 const isDev = !!~['s', 'server'].indexOf(hexo.env.cmd)
 const isGenerate = !!~['g', 'generate'].indexOf(hexo.env.cmd)
+
+const generator = unocss.createGenerator()
+
+/**
+ * 是否开启插件
+ */
+const isEnabled: boolean = get(hexo.config, 'unocss.enabled', false)
+
+/**
+ * css 文件生成路径
+ */
+const cssFile: string = get(hexo.config, 'unocss.file', 'css/uno.css')
 
 // hexo after init
 hexo.extend.filter.register('after_init', async () => {
@@ -19,10 +35,23 @@ hexo.extend.filter.register('after_post_render', async (data) => {
 // 加载 uno.css
 hexo.extend.injector.register('head_end', () => {
   const css = hexo.extend.helper.get('css').bind(hexo)
-  return css('css/uno.css')
+  // hexo 生成 <link />
+  return css(cssFile)
 })
 
 const generateCss = once(async () => {
+  // 未开启
+  if (!isEnabled) {
+    const cssResolvePath = resolve(hexo.source_dir, cssFile)
+
+    // 删除文件
+    if (fs.existsSync(cssResolvePath)) {
+      fs.unlinkSync(cssResolvePath)
+    }
+
+    return Promise.resolve(true)
+  }
+
   if (!isDev && !isGenerate) {
     return Promise.resolve(true)
   }
