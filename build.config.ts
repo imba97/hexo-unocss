553e81218beca276b7a1c459f8ea08f1eeb91a82
imba97 <mail@imba97.cn>
2024-04-12 22:28:51 +0800

    chore: rollup 改为 unbuild

---
+++ build.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path'
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: ['src/index'],
+  clean: true,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+    alias: {
+      entries: {
+        '@': resolve(process.cwd(), 'src')
+      }
+    },
+    esbuild: {
+      // minify: process.env.NODE_ENV === 'production'
+    },
+    inlineDependencies: true
+  }
+})
