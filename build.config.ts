ef31501256adb6bb142911d0ae2f5089940bcbcb
imba97 <mail@imba97.cn>
2024-04-13 00:26:44 +0800

    chore: 0.0.9 版本更新

--- build.config.ts
+++ build.config.ts
@@ -7,6 +7,7 @@ export default defineBuildConfig({
   declaration: true,
   rollup: {
     emitCJS: true,
+    cjsBridge: true,
     alias: {
       entries: {
         '@': resolve(process.cwd(), 'src')
